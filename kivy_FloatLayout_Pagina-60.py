29f12b2dcf127c71cdcb4b4c1c411fc512178636
Manasses.Emanuel <manassesemanuel2@gmail.com>
2024-04-30 18:01:09 -0300

    Kivy Mane

---
+++ kivy_FloatLayout_Pagina-60.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+
+class MinhaApp(App):
+    def build(self):
+
+
+        
+        return layout_float
